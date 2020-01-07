4630f1e84ab6be6b9e6bd69ee664a4c053970c25
Daniel <daniel.d@spot.im>
2020-01-07 17:52:15 +0200

    update slack

--- src/app/reporting/Reporter/slack.js
+++ src/app/reporting/Reporter/slack.js
@@ -12,7 +12,7 @@ export const reportToSlack = (result, summary) => {
         await webhook.send({
             username: 'Bundlesize checker',
             icon_emoji: ':scales:',
-            text: `A new build arrived! ${summary}`,
+            text: `A new build arrived by ${CI_USERNAME}! ${summary}`,
             attachments: [
                 {
                     color: '#008000',
