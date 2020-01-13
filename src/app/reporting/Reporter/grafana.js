228f17e66a8ed204f3b7a05438093abb5ad192e9
Daniel <daniel.d@spot.im>
2020-01-13 12:02:18 +0200

    send branch to grafana

--- src/app/reporting/Reporter/grafana.js
+++ src/app/reporting/Reporter/grafana.js
@@ -2,17 +2,11 @@ import axios from 'axios'
 const CI_BRANCH = process.env.CIRCLE_BRANCH
 
 export const reportToGrafana = result => {
-    let path
-    if (CI_BRANCH) {
-        path = CI_BRANCH + ' ' + result.filePath
-    } else {
-        path = result.filePath
-    }
     const data = {
         driver: 'influxdb-telemetry',
         name: 'bundlesize-monitoring',
         value: result.size / 1024,
-        tags: { path: path },
+        tags: { path: result.filePath, branch: CI_BRANCH },
     }
     axios.post('https://metrics-logger.spot.im/metric', data).catch(error => {
         console.error(`failed to report metric ${error.toString()}`)
